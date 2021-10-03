2f11704b8cc97b65c9b1df4b8dcf4ff88c481539
MRmlik12 <mrmlik12sup@gmail.com>
2021-10-03 23:54:14 +0200

    Add unit tests

--- src/Dcr.Utils/ReadTextMemoryStream.cs
+++ src/Dcr.Utils/ReadTextMemoryStream.cs
@@ -5,12 +5,7 @@ namespace Dcr.Utils
 {
     public static class ReadTextMemoryStream
     {
-        public static MemoryStream GetReadTextMemoryStream(string readedText)
-        {
-            using var memoryStream = new MemoryStream(2048);
-            memoryStream.Write(Encoding.UTF8.GetBytes(readedText), 0, readedText.Length);
-
-            return memoryStream;
-        }
+        public static MemoryStream GetReadTextMemoryStream(string readText)
+            => new MemoryStream(Encoding.ASCII.GetBytes(readText));
     }
 }
